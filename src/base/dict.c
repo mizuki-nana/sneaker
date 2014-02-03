c6d24618659d403c3b82e02114f62c2b7266dd46
Yanzheng Li <yanzheng819@gmail.com>
2014-02-02 19:24:07 -0800

    Added dict.c and associated tests.

---
+++ src/base/dict.c
@@ -0,0 +1,115 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+#include <string.h>
+#include "assert.h"
+#include "dict.h"
+#include "hash.h"
+#include "hashmap.h"
+#include "memory.h"
+#include "utils.h"
+
+#define DICT_DEFAULT_CAPACITY 16
+
+struct HexDict {
+  Hashmap hashmap;
+};
+
+static
+inline int _dict_keycmpfunc(void* key1, void* key2)
+{
+  ASSERT(key1);
+  ASSERT(key2);
+
+  char *_key1 = (char*)key1;
+  char *_key2 = (char*)key2;
+
+  return strcmp(_key1, _key2) == 0;
+}
+
+static
+inline hash_t _dict_hashfunc(void* key)
+{
+  return hash_str((char*)key);
+}
+
+Dict dict_create()
+{
+  Dict dict = MALLOC(struct HexDict);
+
+  if(!dict) {
+    errno = ENOMEM;
+    return NULL;
+  }
+
+  Hashmap hashmap = hashmap_create(
+    DICT_DEFAULT_CAPACITY,
+    _dict_hashfunc,
+    _dict_keycmpfunc
+  );
+
+  if(!hashmap) {
+    errno = ENOMEM;
+    return NULL;
+  }
+
+  dict->hashmap = hashmap;
+
+  return dict;
+}
+
+size_t dict_size(Dict dict)
+{
+  ASSERT(dict);
+  return hashmap_size(dict->hashmap);
+}
+
+void dict_free(Dict *dict)
+{
+  Dict _dict = *dict;
+
+  ASSERT(_dict);
+
+  hashmap_free(&_dict->hashmap);
+
+  FREE(_dict);
+
+  *dict = _dict;
+}
+
+void* dict_put(Dict dict, const char *key, void* val)
+{
+  ASSERT(dict);
+  ASSERT(key);
+  ASSERT(val);
+
+  return hashmap_put(dict->hashmap, (char*)key, val);
+}
+
+void* dict_get(Dict dict, const char *key)
+{
+  ASSERT(dict);
+  ASSERT(key);
+
+  return hashmap_get(dict->hashmap, (char*)key);
+}
---
+++ tests/base/dict_unittest.cc
@@ -0,0 +1,123 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+/* Unit test for src/base/dict.c */
+
+
+#include "../_unittest.h"
+#include "../../src/base/assert.h"
+#include "../../src/base/dict.h"
+#include "../../src/base/memory.h"
+
+class DictTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    _dict = dict_create();
+    ASSERT(_dict);
+  }
+
+  virtual void Teardown() {
+    dict_free(&_dict);
+    ASSERT(_dict == NULL);
+  }
+
+  Dict _dict;
+};
+
+typedef struct {
+  char *key;
+  char *val;
+} KeyVal;
+
+KeyVal fruits[] = {
+  {"a", "apple"},
+  {"b", "banana"},
+  {"c", "coconut"}
+};
+
+KeyVal sky[] = {
+  {"a", "air"},
+  {"b", "balloon"},
+  {"c", "cloud"}
+};
+
+KeyVal vehicles[] = {
+  {"a", "airplane"},
+  {"b", "boat"},
+  {"c", "car"}
+};
+
+
+TEST_F(DictTest, dict_createTest) {
+  ASSERT(_dict);
+  ASSERT_EQ(0, dict_size(_dict));
+}
+
+TEST_F(DictTest, dict_putTest1) {
+  dict_put(_dict, fruits[0].key, fruits[0].val);
+  ASSERT_EQ(1, dict_size(_dict));
+  dict_put(_dict, fruits[1].key, fruits[1].val);
+  ASSERT_EQ(2, dict_size(_dict));
+  dict_put(_dict, fruits[2].key, fruits[2].val);
+  ASSERT_EQ(3, dict_size(_dict));
+
+  char *val1 = (char*)dict_get(_dict, fruits[0].key);
+  char *val2 = (char*)dict_get(_dict, fruits[1].key);
+  char *val3 = (char*)dict_get(_dict, fruits[2].key);
+
+  ASSERT(val1);
+  ASSERT(val2);
+  ASSERT(val3);
+
+  ASSERT_STREQ(val1, fruits[0].val);
+  ASSERT_STREQ(val2, fruits[1].val);
+  ASSERT_STREQ(val3, fruits[2].val);
+}
+
+TEST_F(DictTest, dict_getTest) {
+  dict_put(_dict, fruits[0].key, fruits[0].val);
+  dict_put(_dict, fruits[1].key, fruits[1].val);
+  dict_put(_dict, fruits[2].key, fruits[2].val);
+
+  ASSERT_EQ(3, dict_size(_dict));
+
+  ASSERT_EQ(fruits[0].val, dict_get(_dict, fruits[0].key));
+  ASSERT_EQ(fruits[1].val, dict_get(_dict, fruits[1].key));
+  ASSERT_EQ(fruits[2].val, dict_get(_dict, fruits[2].key));
+
+  ASSERT(dict_put(_dict, sky[1].key, sky[1].val));
+
+  ASSERT_EQ(3, dict_size(_dict));
+
+  ASSERT_EQ(fruits[0].val, dict_get(_dict, fruits[0].key));
+  ASSERT_EQ(sky[1].val, dict_get(_dict, sky[1].key));
+  ASSERT_EQ(fruits[2].val, dict_get(_dict, fruits[2].key));
+
+  ASSERT(dict_put(_dict, vehicles[2].key, vehicles[2].val));
+
+  ASSERT_EQ(3, dict_size(_dict));
+
+  ASSERT_EQ(fruits[0].val, (char*)dict_get(_dict, fruits[0].key));
+  ASSERT_EQ(sky[1].val, (char*)dict_get(_dict, sky[1].key));
+  ASSERT_EQ(vehicles[2].val, (char*)dict_get(_dict, vehicles[2].key));
+}
