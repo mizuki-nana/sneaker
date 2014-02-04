88bfa602fdf7ec7ad5c604953b598f0d725018cb
Yanzheng Li <yanzheng819@gmail.com>
2014-02-03 21:19:04 -0800

    Adding `DaemonService` and `FixedTimeIntervalDaemonService` in src/thread/

---
+++ src/thread/daemon_service.cc
@@ -0,0 +1,54 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+#include "daemon_service.h"
+
+DaemonService::DaemonService(bool wait_for_termination):
+  _wait_for_termination(wait_for_termination)
+{
+  this->init();
+}
+
+void
+DaemonService::init()
+{
+  pthread_attr_init(&_attr);
+  pthread_create(&_thread_id, &_attr, handler, (void*)this);
+
+  // if(_wait_for_termination) {
+  //   void* res = NULL;
+  //   pthread_join(_thread_id, &res);
+  // }
+}
+
+DaemonService::~DaemonService()
+{
+  // Do nothing here.
+}
+
+void*
+DaemonService::handler(void* instance)
+{
+  reinterpret_cast<DaemonService*>(instance)->handle();
+  return NULL;
+}
---
+++ src/thread/fixed_time_interval_daemon_service.cc
@@ -0,0 +1,93 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+// Define `BOOST_SYSTEM_NO_DEPRECATED` to not including deprecated
+// definitions during compilation.
+#define BOOST_SYSTEM_NO_DEPRECATED
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include "fixed_time_interval_daemon_service.h"
+#include "../../src/base/assert.h"
+
+FixedTimeIntervalDaemonService::FixedTimeIntervalDaemonService(
+  size_t interval,
+  ExternalHandler external_handler
+):
+  _interval(interval),
+  _external_handler(external_handler),
+  DaemonService(false)
+{
+  ASSERT(_interval);
+  ASSERT(_external_handler);
+}
+
+FixedTimeIntervalDaemonService::~FixedTimeIntervalDaemonService()
+{
+  // Do nothing here.
+}
+
+size_t
+FixedTimeIntervalDaemonService::interval() const
+{
+  return _interval;
+}
+
+void
+FixedTimeIntervalDaemonService::handle()
+{
+  boost::asio::io_service io;
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(_interval));
+
+  t.async_wait(
+    boost::bind(tick_handler, boost::asio::placeholders::error, &t, this)
+  );
+
+  io.run();
+}
+
+void
+FixedTimeIntervalDaemonService::invoke_external_handler()
+{
+  this->_external_handler();
+}
+
+void
+FixedTimeIntervalDaemonService::tick_handler(
+  const boost::system::error_code& e,
+  boost::asio::deadline_timer* t,
+  FixedTimeIntervalDaemonService* daemon_service
+)
+{
+  t->expires_at(
+    t->expires_at() + boost::posix_time::seconds(daemon_service->interval())
+  );
+
+  t->async_wait(
+    boost::bind(
+      tick_handler, boost::asio::placeholders::error, t, daemon_service)
+  );
+
+  daemon_service->invoke_external_handler();
+}
