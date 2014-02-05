b630c4a357bc6982308f98c2360c4d0e065d7f8d
Yanzheng Li <yanzheng819@gmail.com>
2014-02-04 23:16:34 -0800

    Added src/base/stack.c and tests

---
+++ src/base/stack.c
@@ -0,0 +1,131 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+#include <errno.h>
+#include "assert.h"
+#include "common.h"
+#include "memory.h"
+#include "utils.h"
+#include "stack.h"
+
+
+struct HexStack_s {
+  SinglyNode top;
+  size_t size;
+};
+
+
+Stack stack_create()
+{
+  Stack stack = MALLOC(struct HexStack_s);
+
+  if(stack == NULL) {
+    errno = ENOMEM;
+    return NULL;
+  }
+
+  stack->top = NULL;
+  stack->size = 0;
+
+  return stack;
+}
+
+size_t stack_size(Stack stack)
+{
+  ASSERT(stack);
+  return stack->size;
+}
+
+void* stack_top(Stack stack)
+{
+  ASSERT(stack);
+  RETURN_VAL_IF_NULL(stack->top, NULL);
+  return stack->top->value;
+}
+
+int stack_push(Stack stack, void *val, size_t size)
+{
+  ASSERT(stack);
+
+  RETURN_VAL_IF_NULL(val, 0);
+
+  SinglyNode node = MALLOC(struct SinglyNode_s);
+
+  if(!node) {
+    errno = ENOMEM;
+    return -1;
+  }
+
+  memset(node, 0, sizeof(struct SinglyNode_s));
+
+  node->value = MALLOC_BY_SIZE(size);
+  RETURN_VAL_IF_NULL(node->value, -1);
+
+  memcpy(node->value, val, size);
+  node->next = NULL;
+
+  if(stack->top) {
+    node->next = stack->top;
+  }
+
+  stack->top = node;
+  stack->size++;
+
+  return 1;
+}
+
+void* stack_pop(Stack stack) 
+{
+  ASSERT(stack);
+
+  RETURN_VAL_IF_NULL(stack->top, NULL);
+
+  SinglyNode top = stack->top;
+
+  ASSERT(top);
+  stack->top = top->next;
+
+  void* val = top->value;
+
+  FREE(top);
+  stack->size--;
+
+  return val;
+}
+
+void stack_free(Stack *stack)
+{
+  Stack _stack = *stack;
+  ASSERT(_stack);
+
+  while(stack_size(_stack) > 0) {
+    stack_pop(_stack);
+  }
+
+  ASSERT(_stack->top == NULL);
+  ASSERT(_stack->size == 0);
+
+  FREE(_stack);
+
+  *stack = _stack;
+}
---
+++ tests/base/stack_unittest.cc
@@ -0,0 +1,140 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+/* Unit test for base/stack.c */
+
+
+#include <string.h>
+#include <limits.h>
+#include "../_unittest.h"
+#include "../../src/base/utils.h"
+#include "../../src/base/memory.h"
+#include "../../src/base/assert.h"
+#include "../../src/base/stack.h"
+
+
+class StackTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    _stack = stack_create();
+    ASSERT(_stack);
+  }
+
+  virtual void TearDown() {
+    stack_free(&_stack);
+    ASSERT(_stack == NULL);
+  }
+
+  Stack _stack;
+};
+
+
+TEST_F(StackTest, StackCreationTest) {
+  ASSERT(_stack);
+  ASSERT_EQ(0, stack_size(_stack));
+  ASSERT(stack_pop(_stack) == NULL);
+}
+
+TEST_F(StackTest, StackPushPopTest1) {
+  int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+  int i;
+  for(i = 0; i < 10; i++) {
+    stack_push(_stack, &numbers[i], sizeof(int));
+  }
+
+  ASSERT_EQ(10, stack_size(_stack));
+
+  for(i = 9; i >= 0; i--) {
+    int *p = (int*)stack_pop(_stack);
+    int val = DEREF_VOID(int, p);
+    ASSERT_EQ(numbers[i], val);
+  }
+
+  ASSERT(stack_pop(_stack) == NULL);
+
+  ASSERT_EQ(0, stack_size(_stack));
+}
+
+TEST_F(StackTest, StackPushPopTest2) {
+  int odds[] = {1,3,5,7,9};
+  int evens[] = {2,4,6,8,10};
+  
+  int i;
+  for(i = 0; i < 5; i++) {
+    stack_push(_stack, &odds[i], sizeof(int));
+  }
+
+  ASSERT_EQ(5, stack_size(_stack));
+
+  for(i = 4; i >= 0; i--) {
+    int *p = (int*)stack_pop(_stack);
+    int val = DEREF_VOID(int, p);
+    ASSERT_EQ(odds[i], val);
+  }
+
+  ASSERT(stack_pop(_stack) == NULL);
+  ASSERT_EQ(0, stack_size(_stack));
+
+  for(i = 0; i < 5; i++) {
+    stack_push(_stack, &evens[i], sizeof(int));
+  }
+
+  ASSERT_EQ(5, stack_size(_stack));
+
+  for(i = 4; i >= 0; i--) {
+    int *p = (int*)stack_pop(_stack);
+    int val = DEREF_VOID(int, p);
+    ASSERT_EQ(evens[i], val);
+  }
+
+  ASSERT(stack_pop(_stack) == NULL);
+  ASSERT_EQ(0, stack_size(_stack));
+}
+
+TEST_F(StackTest, StackPushPopTest3) {
+  const char *fruits[26] = {
+    "Apple", "Banana", "Coconut", "Dragonfruit", "Elephant apple",
+    "Finger lime", "Grape", "Honeydrew melon", "Indian prune",
+    "Jasmine", "Kiwi", "Lemon", "Melon", "Nannyberry", "Orange", "Peach",
+    "Quandong", "Raspberry", "Strawberry", "Tangerine", "Uva",
+    "Vanilla", "Watermelon", "Xylocarp", "Yamamomo", "Zucchini"
+  };
+
+  int i;
+  for(i = 0; i < 26; i++) {
+    char *fruit = (char*)fruits[i];
+    stack_push(_stack, fruit, strlen(fruit)+1);
+    ASSERT_EQ(i+1, stack_size(_stack));
+  }
+
+  ASSERT_EQ(26, stack_size(_stack));
+
+  for(i = 25; i >= 0; i--) {
+    char *fruit = (char*)stack_pop(_stack);
+    ASSERT_STREQ(fruit, fruits[i]);
+    ASSERT_EQ(i, stack_size(_stack));
+  }
+
+  ASSERT_EQ(0, stack_size(_stack));
+  ASSERT(stack_pop(_stack) == NULL);
+}
