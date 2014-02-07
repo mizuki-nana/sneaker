d2bec2a0fee0c15ca214f68031900486d78cf5f7
Yanzheng Li <yanzheng819@gmail.com>
2014-02-06 23:40:41 -0800

    Adding `sneaker::container::assorted_value_map`

---
+++ include/container/assorted_value_map.h
@@ -0,0 +1,375 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+/*
+`sneaker::container::assorted_value_map<K, ValueTypes...>` is an associative
+container class where each set of multiple(zero or more) assorted
+statically-typed values are associated to a single statically-typed key.
+
+The types of the keys and values are defined through templating. The type
+of the key must be specified, followed by the assorted types of the values.
+
+Since this is an associative container, its interfaces and usage are very much
+the same as the ones of `std::map`.
+
+Example:
+
+// Define a map of fruits to their scores and descriptions (I'm being very subjective here).
+sneaker::container::assorted_value_map<char*, int, char*> fruits;
+
+fruits.insert("Apple", 100, "Best thing ever <3");
+fruits.insert("Bananna", 90, "Get ALL the Potassum you need!");
+fruits.insert("Orange", 80, "sweet and sour, very citric...");
+
+size_t count = fruits.size();
+std::cout << "There are %d fruits here." << count << std::endl;
+std::cout << "My favorite is Apple: %s (score %d)" << fruits.get<char*, 0>("Apple") \
+    << fruits.get<int, 1>("Apple") << std::endl;
+*/
+
+#ifndef _ASSORTED_MULTIMAP_H_
+#define _ASSORTED_MULTIMAP_H_
+
+#include <map>
+#include <boost/smart_ptr.hpp>
+#include <boost/tuple/tuple.hpp>
+
+
+namespace sneaker {
+
+namespace container {
+
+
+namespace tuple_nms = boost;
+
+
+template<class K, class... ValueTypes>
+class assorted_value_map {
+public:
+  assorted_value_map();
+  ~assorted_value_map();
+
+  /* definitions */
+  using value_type              = typename tuple_nms::tuple<ValueTypes... >;
+  using size_type               = typename std::map<K, value_type>::size_type;
+  using iterator                = typename std::map<K, value_type>::iterator;
+  using const_iterator          = typename std::map<K, value_type>::const_iterator;
+  using reverse_iterator        = typename std::map<K, value_type>::reverse_iterator;
+  using const_reverse_iterator  = typename std::map<K, value_type>::const_reverse_iterator;
+
+  bool empty() const;
+
+  size_type size() const;
+
+  size_type max_size() const;
+
+  void insert(K, ValueTypes...);
+
+  void erase(iterator);
+  size_type erase(const K&);
+  void erase(iterator, iterator);
+
+  void swap(assorted_value_map<K, ValueTypes...>&);
+
+  void clear() noexcept;
+
+        value_type& at(K);
+  const value_type& at(K) const;
+
+  template<class A, size_t Index>
+  A get(K);
+
+  template<class A, size_t Index>
+  const A& get(K) const;
+
+        value_type& operator[](K);
+  const value_type& operator[](K) const;
+
+  iterator begin();
+  const_iterator begin() const;
+
+  iterator end();
+  const_iterator end() const;
+
+  reverse_iterator rbegin();
+  const_reverse_iterator rbegin() const;
+
+  reverse_iterator rend();
+  const_reverse_iterator rend() const;
+
+  const_iterator cbegin() const noexcept; 
+  const_iterator cend() const noexcept;
+
+  const_reverse_iterator crbegin() const noexcept;
+  const_reverse_iterator crend() const noexcept;
+
+  iterator find(K);
+  const_iterator find(K) const;
+
+protected:
+  using core_type = typename std::map<K, value_type>;
+
+  boost::scoped_ptr<core_type> _core;
+};
+
+
+template<class K, class... ValueTypes>
+using _MyType = typename sneaker::container::assorted_value_map<K, ValueTypes...>;
+
+
+template<class K, class... ValueTypes>
+sneaker::container::assorted_value_map<K, ValueTypes...>::assorted_value_map():
+  _core(new std::map<K, tuple_nms::tuple<ValueTypes... >>())
+{
+  ASSERT(_core);
+}
+
+template<class K, class... ValueTypes>
+sneaker::container::assorted_value_map<K, ValueTypes...>::~assorted_value_map()
+{
+  // Do nothing.
+}
+
+template<class K, class... ValueTypes>
+bool
+sneaker::container::assorted_value_map<K, ValueTypes...>::empty() const
+{
+  return _core->empty();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::size_type
+sneaker::container::assorted_value_map<K, ValueTypes...>::size() const
+{
+  return _core->size();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::size_type
+sneaker::container::assorted_value_map<K, ValueTypes...>::max_size() const
+{
+  return _core->max_size();
+}
+
+template<class K, class... ValueTypes>
+void
+sneaker::container::assorted_value_map<K, ValueTypes...>::insert(
+  K key,
+  ValueTypes... values
+)
+{
+  _core->insert(
+    std::pair<K, value_type>(
+      key,
+      value_type(values...)
+    )
+  );
+}
+
+template<class K, class... ValueTypes>
+void
+sneaker::container::assorted_value_map<K, ValueTypes...>::erase(iterator itr)
+{
+  _core->erase(itr);
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::size_type
+sneaker::container::assorted_value_map<K, ValueTypes...>::erase(const K& key)
+{
+  return _core->erase(key);
+}
+
+template<class K, class... ValueTypes>
+void
+sneaker::container::assorted_value_map<K, ValueTypes...>::erase(iterator first, iterator last)
+{
+  _core->erase(first, last);
+}
+
+template<class K, class... ValueTypes>
+void
+sneaker::container::assorted_value_map<K, ValueTypes...>::swap(
+  sneaker::container::assorted_value_map<K, ValueTypes...>& other
+)
+{
+  core_type * other_core = other._core.get();
+  _core->swap(*other_core);
+}
+
+template<class K, class... ValueTypes>
+void
+sneaker::container::assorted_value_map<K, ValueTypes...>::clear() noexcept
+{
+  _core->clear();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::value_type&
+sneaker::container::assorted_value_map<K, ValueTypes...>::at(K key)
+{
+  return static_cast<value_type&>(_core->at(key));
+}
+
+template<class K, class... ValueTypes>
+const typename _MyType<K, ValueTypes...>::value_type&
+sneaker::container::assorted_value_map<K, ValueTypes...>::at(K key) const
+{
+  return static_cast<const value_type&>(_core->at(key));
+}
+
+template<class K, class... ValueTypes>
+template<class A, size_t Index>
+A
+sneaker::container::assorted_value_map<K, ValueTypes...>::get(K key)
+{
+  return boost::get<Index>(at(key));
+}
+
+template<class K, class... ValueTypes>
+template<class A, size_t Index>
+const A&
+sneaker::container::assorted_value_map<K, ValueTypes...>::get(K key) const
+{
+  return boost::get<Index>(at(key));
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::value_type&
+sneaker::container::assorted_value_map<K, ValueTypes...>::operator[](K key)
+{
+  return at(key);
+}
+
+template<class K, class... ValueTypes>
+const typename _MyType<K, ValueTypes...>::value_type&
+sneaker::container::assorted_value_map<K, ValueTypes...>::operator[](K key) const
+{
+  return at(key);
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::begin()
+{
+  return _core->begin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::begin() const
+{
+  return _core->begin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::end()
+{
+  return _core->end();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::end() const
+{
+  return _core->end();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::rbegin()
+{
+  return _core->rbegin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::rbegin() const
+{
+  return _core->rbegin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::rend()
+{
+  return _core->rend();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::rend() const
+{
+  return _core->rend();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::cbegin() const noexcept
+{
+  return _core->cbegin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::cend() const noexcept
+{
+  return _core->cend();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::crbegin() const noexcept
+{
+  return _core->crbegin();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_reverse_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::crend() const noexcept
+{
+  return _core->crend();
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::find(K key)
+{
+  return _core->find(key);
+}
+
+template<class K, class... ValueTypes>
+typename _MyType<K, ValueTypes...>::const_iterator
+sneaker::container::assorted_value_map<K, ValueTypes...>::find(K key) const
+{
+  return _core->find(key);
+}
+
+
+} /* namespace container */
+
+} /* namespace sneaker */
+
+
+#endif /* _ASSORTED_MULTIMAP_H_ */
---
+++ tests/container/assorted_value_map_unittest.cc
@@ -0,0 +1,249 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2014 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+/* Unit test for include/container/assorted_value_map.h */
+
+#include "../_unittest.h"
+#include "../../include/libc/assert.h"
+#include "../../include/container/assorted_value_map.h"
+
+
+using namespace sneaker::container;
+
+
+class AssortedValueMapUnitTestBase : public ::testing::Test {};
+
+
+class AssortedValueMapWithNoValueTypeUnitTest : public AssortedValueMapUnitTestBase {
+protected:
+  virtual void SetUp() {
+    _map = new sneaker::container::assorted_value_map<int>();
+  }
+
+  virtual void TearDown() {
+    delete _map;
+  }
+
+  sneaker::container::assorted_value_map<int> * _map;
+};
+
+
+TEST_F(AssortedValueMapWithNoValueTypeUnitTest, TestInitialization)
+{
+  ASSERT(_map);
+  ASSERT_TRUE(_map->empty());
+  ASSERT_EQ(0, _map->size());
+}
+
+TEST_F(AssortedValueMapWithNoValueTypeUnitTest, TestPutAndAt)
+{
+  _map->insert(1);
+  _map->insert(2);
+  _map->insert(3);
+
+  ASSERT_FALSE(_map->empty());
+  ASSERT_EQ(3, _map->size());
+
+  // boost::tuple<int> t(1);
+  // auto v = boost::get<0>(t);
+  // ASSERT_EQ(1, _map->at(1).get<0>());
+}
+
+
+class AssortedValueMapWithMultiplyValueTypesUnitTest : public AssortedValueMapUnitTestBase {
+protected:
+  virtual void SetUp() {
+    _map = new sneaker::container::assorted_value_map<char, int, long, bool>();
+  }
+
+  virtual void TearDown() {
+    delete _map;
+  }
+
+  sneaker::container::assorted_value_map<char, int, long, bool> * _map;
+};
+
+TEST_F(AssortedValueMapWithMultiplyValueTypesUnitTest, TestInitialization)
+{
+  ASSERT(_map);
+  ASSERT_TRUE(_map->empty());
+  ASSERT_EQ(0, _map->size());
+}
+
+TEST_F(AssortedValueMapWithMultiplyValueTypesUnitTest, TestPutAndAt)
+{
+  _map->insert('a', 1, 100, true);
+  _map->insert('b', 2, 200, true);
+  _map->insert('c', 3, 300, false);
+
+  ASSERT_FALSE(_map->empty());
+  ASSERT_EQ(3, _map->size());
+
+  ASSERT_EQ(1, boost::get<0>(_map->at('a')));
+  ASSERT_EQ(2, boost::get<0>(_map->at('b')));
+  ASSERT_EQ(3, boost::get<0>(_map->at('c')));
+
+  ASSERT_EQ(100, boost::get<1>(_map->at('a')));
+  ASSERT_EQ(200, boost::get<1>(_map->at('b')));
+  ASSERT_EQ(300, boost::get<1>(_map->at('c')));
+
+  ASSERT_EQ(true,  boost::get<2>(_map->at('a')));
+  ASSERT_EQ(true,  boost::get<2>(_map->at('b')));
+  ASSERT_EQ(false, boost::get<2>(_map->at('c')));
+}
+
+TEST_F(AssortedValueMapWithMultiplyValueTypesUnitTest, TestPutAndGet)
+{
+  _map->insert('a', 1, 100, true);
+  _map->insert('b', 2, 200, true);
+  _map->insert('c', 3, 300, false);
+
+  ASSERT_FALSE(_map->empty());
+  ASSERT_EQ(3, _map->size());
+
+  int  a1 = _map->get<int,  0>('a');
+  long a2 = _map->get<long, 1>('a');
+  bool a3 = _map->get<bool, 2>('a');
+
+  int  b1 = _map->get<int,  0>('b');
+  long b2 = _map->get<long, 1>('b');
+  bool b3 = _map->get<bool, 2>('b');
+
+  int  c1 = _map->get<int,  0>('c');
+  long c2 = _map->get<long, 1>('c');
+  bool c3 = _map->get<bool, 2>('c');
+
+  ASSERT_EQ(1, a1);
+  ASSERT_EQ(100, a2);
+  ASSERT_EQ(true, a3);
+
+  ASSERT_EQ(2, b1);
+  ASSERT_EQ(200, b2);
+  ASSERT_EQ(true, b3);
+
+  ASSERT_EQ(3, c1);
+  ASSERT_EQ(300, c2);
+  ASSERT_EQ(false, c3);
+}
+
+TEST_F(AssortedValueMapWithMultiplyValueTypesUnitTest, TestPutAndGetByReference)
+{
+  _map->insert('a', 1, 100, true);
+  _map->insert('b', 2, 200, true);
+  _map->insert('c', 3, 300, false);
+
+  ASSERT_FALSE(_map->empty());
+  ASSERT_EQ(3, _map->size());
+
+  const int  &a1 = _map->get<int,  0>('a');
+  const long &a2 = _map->get<long, 1>('a');
+  const bool &a3 = _map->get<bool, 2>('a');
+
+  const int  &b1 = _map->get<int,  0>('b');
+  const long &b2 = _map->get<long, 1>('b');
+  const bool &b3 = _map->get<bool, 2>('b');
+
+  const int  &c1 = _map->get<int,  0>('c');
+  const long &c2 = _map->get<long, 1>('c');
+  const bool &c3 = _map->get<bool, 2>('c');
+
+  ASSERT_EQ(1, a1);
+  ASSERT_EQ(100, a2);
+  ASSERT_EQ(true, a3);
+
+  ASSERT_EQ(2, b1);
+  ASSERT_EQ(200, b2);
+  ASSERT_EQ(true, b3);
+
+  ASSERT_EQ(3, c1);
+  ASSERT_EQ(300, c2);
+  ASSERT_EQ(false, c3);
+}
+
+
+class AssortedValueMapUnitTest : public AssortedValueMapUnitTestBase {};
+
+
+TEST_F(AssortedValueMapUnitTest, TestSwap)
+{
+  sneaker::container::assorted_value_map<char*, int, bool, char*> map1;
+  sneaker::container::assorted_value_map<char*, int, bool, char*> map2;
+
+  map1.insert("Grape",      60,  false, "Too sour, don't like...");
+  map1.insert("Watermalon", 90,  false, "The best in the summer");
+
+  map2.insert("Apple",      100, false, "best thing ever :-)");
+  map2.insert("Orange",     80,  true,  "I'm lovin' it <3");
+  map2.insert("Bananna",    90,  false, "get ALL the Potassium!!!");
+
+  ASSERT_EQ(2, map1.size());
+  ASSERT_EQ(3, map2.size());
+
+  // do the swap magic...
+  map1.swap(map2);
+
+  ASSERT_EQ(3, map1.size());
+  ASSERT_EQ(2, map2.size());
+
+  // check map1
+  int   map1_a1 = map1.get<int,   0>("Apple");      ASSERT_EQ(map1_a1, 100);
+  bool  map1_a2 = map1.get<bool,  1>("Apple");      ASSERT_EQ(map1_a2, false);
+  char* map1_a3 = map1.get<char*, 2>("Apple");      ASSERT_EQ(map1_a3, "best thing ever :-)");
+
+  int   map1_b1 = map1.get<int,   0>("Orange");     ASSERT_EQ(map1_b1, 80);
+  bool  map1_b2 = map1.get<bool,  1>("Orange");     ASSERT_EQ(map1_b2, true);
+  char* map1_b3 = map1.get<char*, 2>("Orange");     ASSERT_EQ(map1_b3, "I'm lovin' it <3");
+
+  int   map1_c1 = map1.get<int,   0>("Bananna");    ASSERT_EQ(map1_c1, 90);
+  bool  map1_c2 = map1.get<bool,  1>("Bananna");    ASSERT_EQ(map1_c2, false);
+  char* map1_c3 = map1.get<char*, 2>("Bananna");    ASSERT_EQ(map1_c3, "get ALL the Potassium!!!");
+
+  // check map2
+  int   map2_a1 = map2.get<int,   0>("Grape");      ASSERT_EQ(map2_a1, 60);
+  bool  map2_a2 = map2.get<bool,  1>("Grape");      ASSERT_EQ(map2_a2, false);
+  char* map2_a3 = map2.get<char*, 2>("Grape");      ASSERT_EQ(map2_a3, "Too sour, don't like...");
+
+  int   map2_b1 = map2.get<int,   0>("Watermalon"); ASSERT_EQ(map2_b1, 90);
+  bool  map2_b2 = map2.get<bool,  1>("Watermalon"); ASSERT_EQ(map2_b2, false);
+  char* map2_b3 = map2.get<char*, 2>("Watermalon"); ASSERT_EQ(map2_b3, "The best in the summer");
+}
+
+TEST_F(AssortedValueMapUnitTest, TestFind)
+{
+  typedef sneaker::container::assorted_value_map<char*, int, bool> _map_type;
+  _map_type map;
+
+  _map_type::iterator itr = map.find("Apple");
+  ASSERT_EQ(itr, map.end());
+
+  map.insert("Apple", 100, true);
+
+  itr = map.find("Apple");
+  ASSERT_NE(itr, map.end());
+  ASSERT_STREQ("Apple", map.find("Apple")->first);
+
+  _map_type::value_type value = map.find("Apple")->second;
+
+  ASSERT_EQ(100,  value.get<0>());
+  ASSERT_EQ(true, value.get<1>());
+}
