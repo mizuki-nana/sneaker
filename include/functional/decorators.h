81049e6ba8a4d42cc7126d595272da38ffccf6db
Yanzheng Li <yanzheng819@gmail.com>
2014-08-02 21:47:27 -0700

    [SNEAKER-33] Design decorator pattern

--- include/functional/decorators.h
+++ include/functional/decorators.h
@@ -21,9 +21,47 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************************************************************************/
 
-#ifndef SNEAKER_UNIT_TEST_H_
-#define SNEAKER_UNIT_TEST_H_
+#ifndef SNEAKER_DECORATORS_H_
+#define SNEAKER_DECORATORS_H_
 
-#include <gtest/gtest.h>
 
-#endif /* SNEAKER_UNIT_TEST_H_ */
+#include <cassert>
+#include "function.h"
+
+
+namespace sneaker {
+
+
+namespace functional {
+
+
+template<class R, class... Args>
+class retry: public sneaker::functional::function<R, Args...> {
+public:
+  template<class Functor>
+  retry(Functor func) : sneaker::functional::function<R, Args...>(func) {}
+
+  template<class ExceptionType, uint32_t N>
+  R operator() (Args... args) const {
+    for(int i = 0; i < N; ++i) {
+      try {
+        return this->_func(args...);
+      } catch(const ExceptionType&) {
+        continue;
+      } catch(const std::exception& exc) {
+        throw exc;
+      }
+    }
+
+    return this->_func(args...);
+  }
+};
+
+
+} /* end namespace functional */
+
+
+} /* end namespace sneaker */
+
+
+#endif /* SNEAKER_DECORATORS_H_ */
