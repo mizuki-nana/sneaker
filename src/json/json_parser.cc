166ea67383691d61dbf0c583e027c11d8fd44d07
Yanzheng Li <yanzheng819@gmail.com>
2014-09-02 22:06:02 -0700

    [SNEAKER-54] Fix compilation fails in src/json/json_parser.cc

--- src/json/json_parser.cc
+++ src/json/json_parser.cc
@@ -22,6 +22,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************************************************************************/
 
 #include <cassert>
+#include <cstdlib>
 #include <limits>
 #include <map>
 #include <string>
